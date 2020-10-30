f8cfd728090654cce01bd56cbfa23dcf821eae62
ghl998 <73216125+ghl998@users.noreply.github.com>
2020-10-30 23:36:01 +0800

    Add Profile

--- OceanArchiveRevamp/app.tsx
+++ OceanArchiveRevamp/app.tsx
@@ -25,6 +25,7 @@ import CreateItem from './components/createItem';
 import CreateCollection from './components/createCollection';
 import CreateAnnouncement from './components/createAnnouncement';
 import OnBoardingForm from './components/onboardingForm';
+import Profile from './components/Profile';
 import Search from './components/search';
 
 import * as Constant from './constants';
@@ -214,7 +215,18 @@ class Homepage extends React.Component {
                             )
                         }}
                     />
+                    <Route path="/Profile"
+                        render={() => {
+                            return (
+                                <div>
+                                    <Header logIn={(x) => this.logIn(x)} loggedIn={this.state.loggedIn} />
+                                    <Profile />
+                                </div>
+                            )
+                        }}
+                    />
                 </Switch>
+                
             </div>
         );
     }
