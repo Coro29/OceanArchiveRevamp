11697632eadb609df0b26356586c874b49e8f3e2
William Wood <wcw838@uowmail.edu.au>
2020-11-01 09:23:10 +1100

    Fixed up profile page a bit.

--- OceanArchiveRevamp/components/Profile.js
+++ OceanArchiveRevamp/components/Profile.js
@@ -2,121 +2,87 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var React = require('react');
 const reactstrap_1 = require("reactstrap");
-class Profilepart1 extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (React.createElement(reactstrap_1.Row, null,
-            React.createElement(reactstrap_1.Col, { md: 2 },
-                React.createElement("img", { src: 'https://live.staticflickr.com/2490/4214811049_1264c95738_b.jpg', width: '80%' })),
-            React.createElement(reactstrap_1.Col, { md: 2 },
-                React.createElement(reactstrap_1.FormGroup, null,
-                    React.createElement("h1", null, "Hey Admin"),
-                    React.createElement("br", null),
-                    React.createElement(reactstrap_1.Label, { for: "" }, "First Name"),
-                    React.createElement(reactstrap_1.Input, { type: "text", name: "fname", id: "fname", placeholder: "" }))),
-            React.createElement(reactstrap_1.Col, { md: 2 },
-                React.createElement(reactstrap_1.FormGroup, null,
-                    React.createElement("h1", null),
-                    React.createElement("br", null),
-                    React.createElement(reactstrap_1.Label, { for: "" }, "Last Name"),
-                    React.createElement(reactstrap_1.Input, { type: "text", name: "fname", id: "fname", placeholder: "" }))),
-            React.createElement(reactstrap_1.Col, { md: 3 },
-                React.createElement(reactstrap_1.FormGroup, null,
-                    React.createElement("h1", null),
-                    React.createElement("br", null),
-                    React.createElement(reactstrap_1.Label, { for: "" }, "Email"),
-                    React.createElement(reactstrap_1.Input, { type: "text", name: "fname", id: "fname", placeholder: "" }))),
-            React.createElement(reactstrap_1.Col, { md: 3 },
-                React.createElement(reactstrap_1.FormGroup, null,
-                    React.createElement("br", null),
-                    React.createElement("br", null),
-                    React.createElement("br", null),
-                    React.createElement("br", null),
-                    React.createElement("div", { className: 'profileFooterbtn save' }, "Change Password")))));
-    }
-}
-class Profilepart2 extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (React.createElement(reactstrap_1.Row, null,
-            React.createElement(reactstrap_1.Col, { md: 3 },
-                React.createElement(reactstrap_1.FormGroup, null,
-                    React.createElement(reactstrap_1.Label, { for: "" }, "City"),
-                    React.createElement(reactstrap_1.Input, { type: "text", name: "city", id: "city", placeholder: "" }),
-                    React.createElement("br", null),
-                    React.createElement("br", null),
-                    React.createElement("br", null),
-                    React.createElement(reactstrap_1.Label, { for: "" }, "Country"),
-                    React.createElement(reactstrap_1.Input, { type: "text", name: "country", id: "country", placeholder: "" }))),
-            React.createElement(reactstrap_1.Col, { md: 3 },
-                React.createElement(reactstrap_1.FormGroup, null,
-                    React.createElement(reactstrap_1.Label, { for: "" }, "Field of Experties"),
-                    React.createElement(reactstrap_1.Input, { type: "password", name: "field", id: "field", placeholder: "" }),
-                    React.createElement("br", null),
-                    React.createElement("br", null),
-                    React.createElement("br", null),
-                    React.createElement(reactstrap_1.Label, { for: "" }, "Position"),
-                    React.createElement(reactstrap_1.Input, { type: "text", name: "position", id: "position", placeholder: "" }))),
-            React.createElement(reactstrap_1.Col, { md: 6 },
-                React.createElement(reactstrap_1.FormGroup, null,
-                    React.createElement(reactstrap_1.Label, { for: 'Biography' }, "Biography"),
-                    React.createElement(reactstrap_1.Input, { type: 'textarea', name: 'dBiographyesc', id: 'Biography', placeholder: "" })))));
-    }
-}
-class Profilepart3 extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (React.createElement("div", null,
-            React.createElement(reactstrap_1.FormGroup, null,
-                React.createElement(reactstrap_1.Label, { for: 'website' }, "Website"),
-                React.createElement(reactstrap_1.Input, { type: 'text', name: 'website', id: 'website' }),
-                React.createElement(reactstrap_1.FormText, { color: "muted" }, "* Website URL must start with http:// or https://")),
-            React.createElement(reactstrap_1.FormGroup, null,
-                React.createElement(reactstrap_1.Label, { for: 'socialmedia' }, "Social Media (URL)"),
-                React.createElement(reactstrap_1.Input, { type: 'text', name: 'subtitle', id: 'subtitle' }),
-                React.createElement(reactstrap_1.FormText, { color: "muted" }, "* All URL's must start with http:// or https://")),
-            React.createElement(reactstrap_1.FormGroup, null,
-                React.createElement(reactstrap_1.Label, { for: 'affiliation' }, "Affiliation"),
-                React.createElement(reactstrap_1.Input, { type: 'text', name: 'affiliation', id: 'affiliation' })),
-            React.createElement(reactstrap_1.FormGroup, null,
-                React.createElement("div", { style: { display: 'flex' } },
-                    React.createElement("input", { className: 'checkBox', type: 'checkbox', id: '', name: '', value: 'TAC' }),
-                    React.createElement("label", { className: 'checkBoxLabel', for: 'termsAndConditions' }, "Make my profile public")),
-                React.createElement(reactstrap_1.FormText, { color: "muted" }, "By enabling this you accept that all your information will be viewable by the general public."))));
-    }
-}
-class Profilefooter extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (React.createElement(reactstrap_1.ModalFooter, { className: 'profileFooter' },
-            React.createElement("div", { className: 'profileFooterbtn delete', onClick: this.toggle }, "Delete Account"),
-            React.createElement("div", { className: 'fillerBox' }),
-            React.createElement("div", { className: 'profileFooterbtn save' }, "Save")));
-    }
-}
 class Profile extends React.Component {
     constructor(props) {
         super(props);
     }
     render() {
-        return (React.createElement("div", { className: "ICAcontainer abc" },
-            React.createElement("div", { className: 'listSection' },
-                React.createElement("div", { className: 'createItemPage' },
-                    React.createElement(Profilepart1, null),
-                    React.createElement("div", { className: 'profileFooterbtn save' }, "Upload profile picture"),
-                    React.createElement("br", null),
-                    React.createElement("h1", null, "Other Informations:"),
-                    React.createElement(Profilepart2, null),
-                    React.createElement(Profilepart3, null),
-                    React.createElement(Profilefooter, null)))));
+        return (React.createElement("div", { className: 'profileContainer' },
+            React.createElement(reactstrap_1.Row, null,
+                React.createElement(reactstrap_1.Col, { md: 2 },
+                    React.createElement("img", { src: 'https://live.staticflickr.com/2490/4214811049_1264c95738_b.jpg', width: '80%' })),
+                React.createElement(reactstrap_1.Col, { md: 2 },
+                    React.createElement(reactstrap_1.FormGroup, null,
+                        React.createElement("h1", null, "Hey Admin"),
+                        React.createElement("br", null),
+                        React.createElement(reactstrap_1.Label, { for: "" }, "First Name"),
+                        React.createElement(reactstrap_1.Input, { type: "text", name: "fname", id: "fname", placeholder: "" }))),
+                React.createElement(reactstrap_1.Col, { md: 2 },
+                    React.createElement(reactstrap_1.FormGroup, null,
+                        React.createElement("h1", null),
+                        React.createElement("br", null),
+                        React.createElement(reactstrap_1.Label, { for: "" }, "Last Name"),
+                        React.createElement(reactstrap_1.Input, { type: "text", name: "fname", id: "fname", placeholder: "" }))),
+                React.createElement(reactstrap_1.Col, { md: 3 },
+                    React.createElement(reactstrap_1.FormGroup, null,
+                        React.createElement("h1", null),
+                        React.createElement("br", null),
+                        React.createElement(reactstrap_1.Label, { for: "" }, "Email"),
+                        React.createElement(reactstrap_1.Input, { type: "text", name: "fname", id: "fname", placeholder: "" }))),
+                React.createElement(reactstrap_1.Col, { md: 3 },
+                    React.createElement(reactstrap_1.FormGroup, null,
+                        React.createElement("br", null),
+                        React.createElement("br", null),
+                        React.createElement("br", null),
+                        React.createElement("br", null),
+                        React.createElement("div", { className: 'profileFooterbtn save' }, "Change Password")))),
+            React.createElement("div", { className: 'profileFooterbtn photo save' }, "Upload profile picture"),
+            React.createElement("br", null),
+            React.createElement("h1", null, "Other Informations:"),
+            React.createElement(reactstrap_1.Row, null,
+                React.createElement(reactstrap_1.Col, { md: 3 },
+                    React.createElement(reactstrap_1.FormGroup, null,
+                        React.createElement(reactstrap_1.Label, { for: "" }, "City"),
+                        React.createElement(reactstrap_1.Input, { type: "text", name: "city", id: "city", placeholder: "" }),
+                        React.createElement("br", null),
+                        React.createElement("br", null),
+                        React.createElement("br", null),
+                        React.createElement(reactstrap_1.Label, { for: "" }, "Country"),
+                        React.createElement(reactstrap_1.Input, { type: "text", name: "country", id: "country", placeholder: "" }))),
+                React.createElement(reactstrap_1.Col, { md: 3 },
+                    React.createElement(reactstrap_1.FormGroup, null,
+                        React.createElement(reactstrap_1.Label, { for: "" }, "Field of Experties"),
+                        React.createElement(reactstrap_1.Input, { type: "password", name: "field", id: "field", placeholder: "" }),
+                        React.createElement("br", null),
+                        React.createElement("br", null),
+                        React.createElement("br", null),
+                        React.createElement(reactstrap_1.Label, { for: "" }, "Position"),
+                        React.createElement(reactstrap_1.Input, { type: "text", name: "position", id: "position", placeholder: "" }))),
+                React.createElement(reactstrap_1.Col, { md: 6 },
+                    React.createElement(reactstrap_1.FormGroup, null,
+                        React.createElement(reactstrap_1.Label, { for: 'Biography' }, "Biography"),
+                        React.createElement(reactstrap_1.Input, { type: 'textarea', name: 'dBiographyesc', id: 'Biography', placeholder: "" })))),
+            React.createElement("div", null,
+                React.createElement(reactstrap_1.FormGroup, null,
+                    React.createElement(reactstrap_1.Label, { for: 'website' }, "Website"),
+                    React.createElement(reactstrap_1.Input, { type: 'text', name: 'website', id: 'website' }),
+                    React.createElement(reactstrap_1.FormText, { color: "muted" }, "* Website URL must start with http:// or https://")),
+                React.createElement(reactstrap_1.FormGroup, null,
+                    React.createElement(reactstrap_1.Label, { for: 'socialmedia' }, "Social Media (URL)"),
+                    React.createElement(reactstrap_1.Input, { type: 'text', name: 'subtitle', id: 'subtitle' }),
+                    React.createElement(reactstrap_1.FormText, { color: "muted" }, "* All URL's must start with http:// or https://")),
+                React.createElement(reactstrap_1.FormGroup, null,
+                    React.createElement(reactstrap_1.Label, { for: 'affiliation' }, "Affiliation"),
+                    React.createElement(reactstrap_1.Input, { type: 'text', name: 'affiliation', id: 'affiliation' })),
+                React.createElement(reactstrap_1.FormGroup, null,
+                    React.createElement("div", { style: { display: 'flex' } },
+                        React.createElement("input", { className: 'checkBox', type: 'checkbox', id: '', name: '', value: 'TAC' }),
+                        React.createElement("label", { className: 'checkBoxLabel', for: 'termsAndConditions' }, "Make my profile public")),
+                    React.createElement(reactstrap_1.FormText, { color: "muted" }, "By enabling this you accept that all your information will be viewable by the general public."))),
+            React.createElement("div", { className: 'profileFooter' },
+                React.createElement("div", { className: 'profileFooterbtn delete', onClick: this.toggle }, "Delete Account"),
+                React.createElement("div", { className: 'fillerBox' }),
+                React.createElement("div", { className: 'profileFooterbtn save' }, "Save"))));
     }
 }
 exports.default = Profile;
--- OceanArchiveRevamp/components/Profile.tsx
+++ OceanArchiveRevamp/components/Profile.tsx
@@ -2,174 +2,11 @@ declare var require: any
 
 var React = require('react');
 
-import { ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem, Pagination, PaginationItem, PaginationLink, Modal, ModalHeader,Form,
-    FormFeedback,Label,ModalFooter,Input,CustomInput,FormText,Col,Row,
-    FormGroup,} from 'reactstrap';
-
-
-
-class Profilepart1 extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-
-
-            
-                <Row>
-                    <Col md={2}>
-                        <img src='https://live.staticflickr.com/2490/4214811049_1264c95738_b.jpg'  width='80%'></img>
-                    </Col>
-                    
-                    
-                <Col md={2}>
-                <FormGroup>
-                    <h1>Hey Admin</h1>
-                    <br></br>  
-                <Label for="">First Name</Label>
-                <Input type="text" name="fname" id="fname" placeholder="" />
-                </FormGroup>
-                </Col>
-
-
-                <Col md={2}>
-                <FormGroup>
-                    <h1></h1>
-                    <br></br>
-                <Label for="">Last Name</Label>
-                <Input type="text" name="fname" id="fname" placeholder="" />
-                </FormGroup>
-                </Col>
-
-                <Col md={3}>
-                <FormGroup>
-                    <h1></h1>
-                    <br></br>
-                <Label for="">Email</Label>
-                <Input type="text" name="fname" id="fname" placeholder="" />
-                </FormGroup>
-                </Col>
-
-                <Col md={3}>
-                <FormGroup>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <div className='profileFooterbtn save'>Change Password</div>
-                </FormGroup>
-                </Col>
-                       
-
-                </Row>
-            
-            
-        );
-    }
-}
-
-class Profilepart2 extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            
-            <Row>
-            <Col md={3}>
-              <FormGroup>
-                <Label for="">City</Label>
-                <Input type="text" name="city" id="city" placeholder="" />
-                <br></br>
-                <br></br>
-                <br></br>
-                <Label for="">Country</Label>
-                <Input type="text" name="country" id="country" placeholder="" />
-              </FormGroup>
-            </Col>
-            <Col md={3}>
-              <FormGroup>
-                <Label for="">Field of Experties</Label>
-                <Input type="password" name="field" id="field" placeholder=""/>
-                <br></br>
-                <br></br>
-                <br></br>
-                <Label for="">Position</Label>
-                <Input type="text" name="position" id="position" placeholder=""/>
-              </FormGroup>
-            </Col>
-            <Col md={6}>
-                <FormGroup>
-                    <Label for='Biography'>Biography</Label>
-                    <Input type='textarea' name='dBiographyesc' id='Biography' placeholder="" />
-                </FormGroup>
-            </Col>
-          </Row>
-         
-
-
-
-
-        );
-    }
-}
-
-class Profilepart3 extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <div >
-                <FormGroup>
-                    <Label for='website'>Website</Label>
-                    <Input type='text' name='website' id='website'  />
-                    <FormText color="muted">* Website URL must start with http:// or https://</FormText>
-                </FormGroup>
-                <FormGroup>
-                    <Label for='socialmedia'>Social Media (URL)</Label>
-                    <Input type='text' name='subtitle' id='subtitle'  /> 
-                    <FormText color="muted">* All URL's must start with http:// or https://</FormText>
-                </FormGroup>
-                <FormGroup>
-                    <Label for='affiliation'>Affiliation</Label>
-                    <Input type='text' name='affiliation' id='affiliation'  />
-                </FormGroup>
-                <FormGroup>
-                <div style={{ display: 'flex' }}>
-                    <input className='checkBox' type='checkbox' id='' name='' value='TAC' />
-                    <label className='checkBoxLabel' for='termsAndConditions'>Make my profile public</label>
-                    </div>
-                    <FormText color="muted">By enabling this you accept that all your information will be viewable by the general public.</FormText>
-                </FormGroup>
-            </div>
-        );
-    }
-}
-
-class Profilefooter extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <ModalFooter className='profileFooter'>
-            <div className='profileFooterbtn delete' onClick={this.toggle}>Delete Account</div>
-            <div className='fillerBox' />
-            <div className='profileFooterbtn save'>Save</div>
-            </ModalFooter>
-        );
-    }
-}
-
-
-
-
+import {
+    ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem, Pagination, PaginationItem, PaginationLink, Modal, ModalHeader, Form,
+    FormFeedback, Label, ModalFooter, Input, CustomInput, FormText, Col, Row,
+    FormGroup,
+} from 'reactstrap';
 
 export default class Profile extends React.Component {
     constructor(props) {
@@ -177,28 +14,110 @@ export default class Profile extends React.Component {
 
     }
 
-
-
-
     render() {
-        
+
         return (
-            <div className="ICAcontainer abc">
-                
-                
-                <div className='listSection'>
-                <div className='createItemPage'>
-                    <Profilepart1 /> 
-                    <div className='profileFooterbtn save'>Upload profile picture</div>
-                    <br></br>
-                    <h1>Other Informations:</h1>
-                    <Profilepart2 />
-                    <Profilepart3 />
-                    <Profilefooter />
-                    </div>
+            <div className='profileContainer'>
+                <Row>
+                    <Col md={2}>
+                        <img src='https://live.staticflickr.com/2490/4214811049_1264c95738_b.jpg' width='80%'></img>
+                    </Col>
+                    <Col md={2}>
+                        <FormGroup>
+                            <h1>Hey Admin</h1>
+                            <br></br>
+                            <Label for="">First Name</Label>
+                            <Input type="text" name="fname" id="fname" placeholder="" />
+                        </FormGroup>
+                    </Col>
+                    <Col md={2}>
+                        <FormGroup>
+                            <h1></h1>
+                            <br></br>
+                            <Label for="">Last Name</Label>
+                            <Input type="text" name="fname" id="fname" placeholder="" />
+                        </FormGroup>
+                    </Col>
+                    <Col md={3}>
+                        <FormGroup>
+                            <h1></h1>
+                            <br></br>
+                            <Label for="">Email</Label>
+                            <Input type="text" name="fname" id="fname" placeholder="" />
+                        </FormGroup>
+                    </Col>
+                    <Col md={3}>
+                        <FormGroup>
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                            <div className='profileFooterbtn save'>Change Password</div>
+                        </FormGroup>
+                    </Col>
+                </Row>
+                <div className='profileFooterbtn photo save'>Upload profile picture</div>
+                <br></br>
+                <h1>Other Informations:</h1>
+                <Row>
+                    <Col md={3}>
+                        <FormGroup>
+                            <Label for="">City</Label>
+                            <Input type="text" name="city" id="city" placeholder="" />
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                            <Label for="">Country</Label>
+                            <Input type="text" name="country" id="country" placeholder="" />
+                        </FormGroup>
+                    </Col>
+                    <Col md={3}>
+                        <FormGroup>
+                            <Label for="">Field of Experties</Label>
+                            <Input type="password" name="field" id="field" placeholder="" />
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                            <Label for="">Position</Label>
+                            <Input type="text" name="position" id="position" placeholder="" />
+                        </FormGroup>
+                    </Col>
+                    <Col md={6}>
+                        <FormGroup>
+                            <Label for='Biography'>Biography</Label>
+                            <Input type='textarea' name='dBiographyesc' id='Biography' placeholder="" />
+                        </FormGroup>
+                    </Col>
+                </Row>
+                <div >
+                    <FormGroup>
+                        <Label for='website'>Website</Label>
+                        <Input type='text' name='website' id='website' />
+                        <FormText color="muted">* Website URL must start with http:// or https://</FormText>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for='socialmedia'>Social Media (URL)</Label>
+                        <Input type='text' name='subtitle' id='subtitle' />
+                        <FormText color="muted">* All URL's must start with http:// or https://</FormText>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for='affiliation'>Affiliation</Label>
+                        <Input type='text' name='affiliation' id='affiliation' />
+                    </FormGroup>
+                    <FormGroup>
+                        <div style={{ display: 'flex' }}>
+                            <input className='checkBox' type='checkbox' id='' name='' value='TAC' />
+                            <label className='checkBoxLabel' for='termsAndConditions'>Make my profile public</label>
+                        </div>
+                        <FormText color="muted">By enabling this you accept that all your information will be viewable by the general public.</FormText>
+                    </FormGroup>
                 </div>
-                
-            </div >
+                <div className='profileFooter'>
+                    <div className='profileFooterbtn delete' onClick={this.toggle}>Delete Account</div>
+                    <div className='fillerBox' />
+                    <div className='profileFooterbtn save'>Save</div>
+                </div>
+            </div>
         );
     }
 }
